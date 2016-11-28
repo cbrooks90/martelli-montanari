834535ed720b978fc3f7f24fa8053967d1590cfd
Christopher Brooks <flavenstein@gmail.com>
2016-11-28 14:07:52 -0700

    Restructured, removing set operations and stuff

--- unification-mm.scm
+++ unification-mm.scm
@@ -8,17 +8,9 @@
 (define multieqn-vars cadr)
 (define multieqn-rhs cddr)
 
-(define (sorted-union l1 l2 accum)
-  (cond [(null? l1) (append accum l2)]
-        [(null? l2) (append accum l1)]
-        [(< (car l1) (car l2)) (sorted-union (cdr l1) l2 (append accum (list (car l1))))]
-        [(> (car l1) (car l2)) (sorted-union l1 (cdr l2) (append accum (list (car l2))))]
-        [else (sorted-union (cdr l1) (cdr l2) (append accum (list (car l1))))]))
-
-; The possible returns here should correspond to the dispatch cases in "factor"
 (define (prefix term)
   (cond [(null? term) '()]
-        [(pair? term) #t]; This is bad, should use 'cons but 'cons could be a user symbol
+        [(pair? term) #t]; Something to distinguish this case from a symbol prefix
         [(integer? term) term]
         [(symbol? term) term]
         [else (error 'prefix "Unrecognized term")]))
@@ -30,52 +22,52 @@
           [(eqv? last (prefix (car terms))) (loop (cdr terms) last)]
           [else #f])))
 
-(define (factor terms)
+(define (find-equation var eqns)
+  (let loop ([eqns eqns])
+    (cond [(null? eqns) #f]
+          [(eqv? var (multieqn-vars (car eqns))) (car eqns)]
+          [else (loop (cdr eqns))])))
+
+(define (rep v equiv-vars)
+  (let loop ([vs equiv-vars])
+    (cond [(null? vs) v]
+          [(memv v vs) (car vs)]
+          [else (loop (cdr vs))])))
+
+(define (canonicalize vars rhs eqns equiv-vars)
+  (let loop ([vars vars]
+             [refs 0]
+             [reps '()]
+             [rhs rhs]
+             [eqns eqns]
+             [equiv-to-v '()]
+             [equiv-vars equiv-vars])
+    (if (null? vars)
+        (values (var (car reps))
+                (if (eqv? (car reps) 'aux) eqns (cons (multieqn refs (car reps) rhs) eqns))
+                (cons (append reps equiv-to-v) equiv-vars))
+        (let* ([v (rep (var-num (car vars)) equiv-vars)]
+               [eqn (find-equation v eqns)])
+          (loop (cdr vars)
+                (- (+ refs (multieqn-count eqn)) 1)
+                (if (memv v reps) reps (cons v reps))
+                (append rhs (multieqn-rhs eqn))
+                (remove eqn eqns)
+                (append equiv-to-v (cdr (or (assv v equiv-vars) (list #f))))
+                (remove (assv v equiv-vars) equiv-vars))))))
+
+(define (factor terms eqns equiv-vars)
   (let ([vars (filter var? terms)])
     (if (null? vars)
         (let ([head (common-prefix terms)])
           (case head
             [(#t)
-             (let-values ([(car-c car-f) (factor (map car terms))]
-                          [(cdr-c cdr-f) (factor (map cdr terms))])
-               (values (and car-c cdr-c (cons car-c cdr-c))
-                       (append car-f cdr-f)))]
-            [else (values head '())]))
-        (values (car vars)
-                (list (multieqn (- (length vars))
-                                (sort < (map var-num vars))
-                                (filter (lambda (x) (not (var? x))) terms)))))))
-
-(define (empty-intersect? l1 l2)
-  (cond [(or (null? l1) (null? l2)) #t]
-        [(< (car l1) (car l2)) (empty-intersect? (cdr l1) l2)]
-        [(> (car l1) (car l2)) (empty-intersect? l1 (cdr l2))]
-        [else #f]))
-
-(define (merge-eqn e es)
-  (let loop ([es es]
-             [meets '()]
-             [new-count (multieqn-count e)]
-             [new-vars (multieqn-vars e)]
-             [new-rhs (multieqn-rhs e)])
-    (cond [(null? es) (values meets (multieqn new-count new-vars new-rhs))]
-          [(empty-intersect? (multieqn-vars e) (multieqn-vars (car es)))
-           (loop (cdr es) meets new-count new-vars new-rhs)]
-          [else (loop (cdr es)
-                      (cons (car es) meets)
-                      (+ (multieqn-count (car es)) new-count)
-                      (sorted-union new-vars (multieqn-vars (car es)) '())
-                      (append new-rhs (multieqn-rhs (car es))))])))
-
-; For every eqn x in l1, collect all variables and rhs's from equations whose
-; vars have nonempty intersection with vars in x. Make a new equation from those
-; vars+rhs's, then delete the originals
-(define (compact-union l1 l2)
-  (fold-left
-    (lambda (y x)
-      (let-values ([(touched-eqns new-eqn) (merge-eqn x y)])
-        (cons new-eqn (fold-left (lambda (y x) (remove x y)) y touched-eqns))))
-    l2 l1))
+             (let*-values
+               ([(car-c eqns~ equiv-vars~) (factor (map car terms) eqns equiv-vars)]
+                [(cdr-c eqns~ equiv-vars~) (factor (map cdr terms) eqns~ equiv-vars~)])
+               (values (and car-c cdr-c (cons car-c cdr-c)) eqns~ equiv-vars~))]
+            [else (values head eqns equiv-vars)]))
+        (canonicalize vars (filter (lambda (x) (not (var? x))) terms) eqns equiv-vars))))
 
 (define (merge-vars l1 l2 accum)
   (cond [(null? l1) (append accum l2)]
@@ -94,30 +86,56 @@
 ; An invariant of solved sets of multieqns for this algorithm is any variable in
 ; the lhs of an equation is only referred to by an equation after it, not before
 
-(define (solve unsolved solved vars-in-solved)
-  (if (null? unsolved) solved
+(define (solve unsolved solved vars-in-solved equiv-vars)
+  (if (null? unsolved) (values solved equiv-vars)
       (let ([meqn (assv 0 unsolved)])
         (and meqn (let* ([vars (multieqn-vars meqn)]
                          [rhs (multieqn-rhs meqn)]
-                         [new-count (fold-left (lambda (y x) (if (memv (car x) vars)
-                                                                 (+ (cdr x) y) y)) 0 vars-in-solved)])
+                         [new-count (fold-left
+                                      (lambda (y x)
+                                        (if (memv (car x) vars)
+                                            (+ (cdr x) y) y))
+                                      0 vars-in-solved)])
                     (if (null? rhs)
                         (solve (remove meqn unsolved)
                                (cons (multieqn new-count vars rhs) solved)
-                               vars-in-solved)
-                        (let-values ([(c f) (factor rhs)])
-                          (and c (if (eqv? (car vars) 'aux)
-                                     (solve (compact-union f (remove meqn unsolved))
-                                            solved vars-in-solved)
-                                     (solve (compact-union f (remove meqn unsolved))
+                               vars-in-solved
+                               equiv-vars)
+                        (let-values ([(c eqns~ equiv-vars~)
+                                      (factor rhs (remove meqn unsolved) equiv-vars)])
+                          (and c (if (eqv? vars 'aux)
+                                     (solve eqns~ solved vars-in-solved equiv-vars~)
+                                     (solve eqns~
                                             (cons (multieqn new-count vars (list c)) solved)
-                                            (merge-vars (vars-in c) vars-in-solved '())))))))))))
+                                            (merge-vars (vars-in c) vars-in-solved '())
+                                            equiv-vars~))))))))))
 
-(define (unify t1 t2 s)
-  (solve (cons
-           (multieqn 0 `(aux) (list t1 t2))
-           (compact-union
-             (map (lambda (x) (multieqn (cdr x) (list (car x)) '()))
-                  (merge-vars (vars-in t1) (vars-in t2) '()))
-             s))
-         '() '()))
+(define (initialize-eqns eqns var-counts equiv-vars)
+  (if (null? var-counts) eqns
+      (let ([v (rep (caar var-counts) equiv-vars)])
+        (let loop ([eqns eqns] [accum '()])
+          (cond [(null? eqns)
+                 (initialize-eqns
+                   (cons (multieqn (cdar var-counts) v '()) accum)
+                   (cdr var-counts)
+                   equiv-vars)]
+                [(eqv? v (multieqn-vars (car eqns)))
+                 (initialize-eqns
+                   (append accum
+                           (multieqn (+ (cdar var-counts) (multieqn-count (car eqn)))
+                                     v
+                                     (multieqn-rhs (car eqn)))
+                           (cdr eqns))
+                   (cdr var-counts)
+                   equiv-vars)]
+                [else (loop (cdr eqns) (cons (car eqns) accum))])))))
+
+(define (unify t1 t2 prev-solved equiv-vars)
+  (let ([var-counts (merge-vars (vars-in t1) (vars-in t2) '())])
+    (solve (cons
+             (multieqn 0 'aux (list t1 t2))
+             (initialize-eqns prev-solved var-counts equiv-vars))
+           '() '() '())))
+
+(trace unify rep find-equation initialize-eqns solve factor canonicalize)
+(unify `(,(var 0) ,(var 1)) `(5 7) '() '())
