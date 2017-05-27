d6f6c7f55320cb9187072acba266a9b8547dc69b
Chris Brooks <flavenstein@gmail.com>
2017-05-27 15:59:39 -0500

    Move tests into an existing test file, remove redundant 'test-check' definition

--- microKanren-test.scm
+++ microKanren-test.scm
@@ -66,4 +66,89 @@
   (map reify-1st (take 1 (many-non-ans empty-state)))
   '(3))
 
+(test-check "run*"
+  (run* (q) (fresh (x y) (== `(,x ,y) q) (appendo x y '(1 2 3 4 5))))
+  '((() (1 2 3 4 5))
+    ((1) (2 3 4 5))
+    ((1 2) (3 4 5))
+    ((1 2 3) (4 5))
+    ((1 2 3 4) (5))
+    ((1 2 3 4 5) ())))
+
+(test-check "run*2"
+  (run* (q x y) (== `(,x ,y) q) (appendo x y '(1 2 3 4 5)))
+  '((() (1 2 3 4 5))
+    ((1) (2 3 4 5))
+    ((1 2) (3 4 5))
+    ((1 2 3) (4 5))
+    ((1 2 3 4) (5))
+    ((1 2 3 4 5) ())))
+
+(test-check "rember*o"
+  (letrec
+    ((rember*o
+       (lambda (tr o)
+         (conde
+           ((== '() tr) (== '() o))
+           ((fresh
+              (a d)
+              (== `(,a . ,d) tr)
+              (conde
+                ((fresh
+                   (aa da)
+                   (== `(,aa . ,da) a)
+                   (fresh
+                     (a^ d^)
+                     (rember*o a a^)
+                     (rember*o d d^)
+                     (== `(,a^ . ,d^) o))))
+                ((== a 8) (rember*o d o))
+                ((fresh
+                   (d^)
+                   (rember*o d d^)
+                   (== `(,a . ,d^) o))))))))))
+    (run 8 (q) (rember*o q '(1 2 8 3 4 5))))
+  '((1 2 8 3 4 5)
+    (1 2 8 3 4 5 8)
+    (1 2 8 3 4 8 5)
+    (1 2 8 3 8 4 5)
+    (1 2 8 8 3 4 5)
+    (1 2 8 8 3 4 5)
+    (1 8 2 8 3 4 5)
+    (8 1 2 8 3 4 5)))
+
+(test-check "rember*o"
+  (letrec
+    ((rember*o
+       (lambda (tr o)
+         (conde
+           ((== '() tr) (== '() o))
+           ((fresh
+              (a d)
+              (== `(,a . ,d) tr)
+              (conde
+                ((fresh
+                   (aa da)
+                   (== `(,aa . ,da) a)
+                   (fresh
+                     (a^ d^)
+                     (== `(,a^ . ,d^) o)
+                     (rember*o d d^)
+                     (rember*o a a^))))
+                ((== a 8) (rember*o d o))
+                ((fresh
+                   (d^)
+                   (== `(,a . ,d^) o)
+                   (rember*o d d^))))))))))
+    (run 9 (q) (rember*o q '(1 (2 8 3 4) 5))))
+  '((1 (2 8 3 4) 5)
+    (1 (2 8 3 4) 5 8)
+    (1 (2 8 3 4) 5 8 8)
+    (1 (2 8 3 4) 8 5)
+    (1 8 (2 8 3 4) 5)
+    (8 1 (2 8 3 4) 5)
+    (1 (2 8 3 4) 5 8 8 8)
+    (1 (2 8 3 4) 5 8 8 8 8)
+    (1 (2 8 3 4) 5 8 8 8 8 8)))
+
 (load "more-tests.scm")
